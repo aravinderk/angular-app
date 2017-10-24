431d80600dc5c40d462ff90aeb83b822e250926e
aravinderk <aravinder.k@gmail.com>
2017-10-24 10:58:11 +0530

    Added form validation

--- partials/admin/admin-controller.js
+++ partials/admin/admin-controller.js
@@ -4,25 +4,40 @@ AdminController.$inject = ['$scope', 'AdminService', '$state'];
 
 function AdminController($scope, AdminService, $state){
 	// Variables in $scope 
-	$scope.menuItems = AdminService.menuItems;
+	$scope.statusMenuItems = AdminService.statusMenuItems;
+	$scope.leftMenuItems = AdminService.leftMenuItems;
 	$scope.routes = AdminService.routes;
 	$scope.pageIndex = $scope.routes.indexOf($state.current.name) || 0;
 	$scope.showSpaceImg = false;
-	
+	$scope.formData = {
+		basicInfo: {},
+		facilityAndTags: {},
+		addOns: {},
+		engineInfo: {},
+		configuration: {},
+		timings: {},
+		policies: {},
+		promotions: {}
+	};
+
 	// Functions in $scope
 	$scope.handleNext = HandleNext;
 	$scope.saveAsDraft = SaveAsDraft;
 
 	function HandleNext () {
-		if (($scope.routes.length - 1) <= $scope.pageIndex) {
-			return;
+		if($scope.spaceForm.$valid){
+			console.log('form valid');
+			if (($scope.routes.length - 1) <= $scope.pageIndex) {
+				return;
+			}
+			if ($scope.pageIndex === 1 && !$scope.showSpaceImg ) {
+				$scope.showSpaceImg = true;
+				return;
+			}
+			console.log('form data', $scope.formData);
+			$scope.pageIndex++;
+			$state.go($scope.routes[$scope.pageIndex], {}, { location: true });
 		}
-		if ($scope.pageIndex === 1 && !$scope.showSpaceImg ) {
-			$scope.showSpaceImg = true;
-			return;
-		}
-		$scope.pageIndex++;
-		$state.go($scope.routes[$scope.pageIndex], {}, { location: true });
 	}
 
 	function SaveAsDraft () {
