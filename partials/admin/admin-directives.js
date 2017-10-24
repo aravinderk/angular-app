318b765644306af1b87caa6438cd0ecf490f0a2a
aravinderk <aravinder.k@gmail.com>
2017-10-24 17:05:12 +0530

    Added nwook-filter-dropdown directive

--- partials/admin/admin-directives.js
+++ partials/admin/admin-directives.js
@@ -1,5 +1,6 @@
 angular.module('AdminDirectiveModule', [])
-	.directive('nwookDropdown', nwookDropdown);
+	.directive('nwookDropdown', nwookDropdown)
+	.directive('nwookFilterDropdown', nwookFilterDropdown)
 
 function nwookDropdown () {
 	return {
@@ -24,6 +25,48 @@ function nwookDropdown () {
 					'</button>'+
 					'<ul class="dropdown-menu" aria-labelledby="{{id}}">'+
 						'<li ng-repeat="item in items"><a href="#" ng-click="change($event, item)">{{item}}</a></li>'+
+					'</ul>'+
+				'</div>'+
+				'<input type="text" name="{{name}}" ng-model="model" hidden ng-required="{{required}}" />'
+	};
+}
+
+function nwookFilterDropdown () {
+	return {
+		restrict: 'E',
+		scope: {
+			items: '=',
+			id: '@',
+			name: '@',
+			required: '=',
+			model: '=model'
+		},
+		controller: function ($scope) {
+			$scope.selectedItems = [];
+			$scope.change = function (e, item) {
+				e.preventDefault();
+				if($scope.model.indexOf(item) <= -1){
+					$scope.selectedItems.push(item);
+				}
+				$scope.model = $scope.selectedItems;
+			}
+
+			$scope.removeTag = function (item) {
+				var index = $scope.selectedItems.indexOf(item);
+				$scope.selectedItems.splice(index, 1);
+				$scope.model = $scope.selectedItems;
+			}
+		},
+		template: '<div class="dropdown nwook-dropdown">'+
+					'<div class="dropdown-toggle" id="{{id}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'+
+						'<span class="dropdown-tag" ng-repeat="item in model">'+
+							'{{item}} <span class="tag-close" ng-click="removeTag(item)">X</span>'+
+						'</span>'+	
+						'<input type="text" ng-model="searchStr" />'+
+						'<span class="caret"></span>'+
+					'</div>'+
+					'<ul class="dropdown-menu" aria-labelledby="{{id}}">'+
+						'<li ng-repeat="item in items | filter: searchStr" ng-class="{\'disabled\': model.indexOf(item) > -1}"><a href="#" ng-click="change($event, item)">{{item}}</a></li>'+
 					'</ul>'+
 				'</div>'+
 				'<input type="text" name="{{name}}" ng-model="model" hidden ng-required="{{required}}" />'
