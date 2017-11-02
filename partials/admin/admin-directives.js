c75431b5401d8df8bef7d6b8a12fda7e04acd798
Aravinder Reddy K <aravinder.k@gmail.com>
2017-11-02 16:33:11 +0530

    Update admin-directives.js

--- partials/admin/admin-directives.js
+++ partials/admin/admin-directives.js
@@ -2,7 +2,8 @@ angular.module('AdminDirectiveModule', [])
 	.directive('nwookDropdown', nwookDropdown)
 	.directive('nwookFilterDropdown', nwookFilterDropdown)
 	.directive('nwookCounter', nwookCounter)
-	.directive('nwookInputDropdown', nwookInputDropdown);
+	.directive('nwookInputDropdown', nwookInputDropdown)
+	.directive('nwookSelectableButton', nwookSelectableButton);
 
 function nwookDropdown () {
 	return {
@@ -12,17 +13,21 @@ function nwookDropdown () {
 			id: '@',
 			name: '@',
 			required: '=',
-			model: '='
+			model: '=',
+			onchange: '&'
 		},
-		controller: function ($scope) {
+		controller: function ($scope, $timeout) {
+			$scope.selectedVal = '';
 			$scope.change = function (e, item) {
 				e.preventDefault();
+				$scope.selectedVal = item.name
 				$scope.model = item.id;
+				$scope.onchange && $timeout($scope.onchange);
 			}
 		},
 		template: '<div class="dropdown nwook-dropdown">'+
 					'<button class="dropdown-toggle" id="{{id}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'+
-						'<span>{{model}}</span>'+
+						'<span>{{ model && selectedVal}}</span>'+
 						'<span class="caret"></span>'+
 					'</button>'+
 					'<ul class="dropdown-menu" aria-labelledby="{{id}}">'+
@@ -47,28 +52,59 @@ function nwookFilterDropdown () {
 			$scope.selectedItems = [];
 			$scope.change = function (e, item) {
 				e.preventDefault();
-				if($scope.model.indexOf(item) <= -1){
+				if($scope.model.indexOf(item.id) <= -1){
 					$scope.selectedItems.push(item);
 				}
-				$scope.model = $scope.selectedItems;
+				updateModel(); // Update form data
+				$scope.searchStr = ''; //Empty search string in input
+				angular.element(e.target).closest('.nwook-dropdown').find('input[type=text]').focus(); // focus search input
 			}
 
-			$scope.removeTag = function (item) {
-				var index = $scope.selectedItems.indexOf(item);
+			$scope.handleInputChange = function (e) {
+				var _this = angular.element(e.target);
+				var dropdown_btn = _this.closest('.dropdown-toggle');
+				var dropdown = _this.closest('.nwook-dropdown');
+				if(e.keyCode == '40') {
+					dropdown.find('ul li:first-child').find('a').focus();   // focus first element in dropdown when pressing down arrow in search input
+				} else if($scope.searchStr && $scope.searchStr.trim() && !dropdown.hasClass('open')){
+					dropdown_btn.click();
+				} else if (!$scope.searchStr && dropdown.hasClass('open')) {
+					dropdown_btn.click();
+				}
+			}
+
+			$scope.handleKeydown = function (e) {
+				if(e.keyCode == '8' && !$scope.searchStr && $scope.selectedItems.length > 0) {
+					$scope.selectedItems.pop();  // removing selected tag when pressing back button.
+					updateModel();
+				}
+			}
+
+			$scope.removeTag = function (e, item) {   // To remove selected tag when clicking on close icon.
+				var index = $scope.selectedItems.indexOf(item.id);
 				$scope.selectedItems.splice(index, 1);
-				$scope.model = $scope.selectedItems;
+				updateModel();
+				e.stopPropagation();
+			}
+
+			function updateModel () {	// Updating model
+				var selectedItemIds = [];
+				$scope.selectedItems.forEach(function(category){
+					selectedItemIds.push(category.id);
+				});
+				$scope.model = selectedItemIds;
 			}
 		},
 		template: '<div class="dropdown nwook-dropdown">'+
 					'<div class="dropdown-toggle" id="{{id}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'+
-						'<span class="dropdown-tag" ng-repeat="item in model">'+
-							'{{item.name}} <span class="tag-close" ng-click="removeTag(item)">X</span>'+
+						'<span class="dropdown-tag" ng-repeat="item in selectedItems">'+
+							'{{item.name}} <span class="tag-close" ng-click="removeTag($event, item)">X</span>'+
 						'</span>'+	
-						'<input type="text" ng-model="searchStr" />'+
+						'<input type="text" ng-model="searchStr" ng-keyup="handleInputChange($event)" ng-keydown="handleKeydown($event)" />'+
 						'<span class="caret"></span>'+
 					'</div>'+
 					'<ul class="dropdown-menu" aria-labelledby="{{id}}">'+
-						'<li ng-repeat="item in items | filter: searchStr" ng-class="{\'disabled\': model.indexOf(item) > -1}"><a href="#" ng-click="change($event, item)">{{item.name}}</a></li>'+
+						'<li ng-repeat="item in items | filter: searchStr" ng-class="{\'disabled\': model.indexOf(item.id) > -1}"><a href="#" ng-click="change($event, item)">{{item.name}}</a></li>'+
 					'</ul>'+
 				'</div>'+
 				'<input type="text" name="{{name}}" ng-model="model" hidden ng-required="{{required}}" />'
@@ -120,26 +156,54 @@ function nwookInputDropdown () {
 			inputModel: '=',
 			dropdownModel: '=',
 			dropdownItems: '=',
-			required: '='
+			required: '=',
+			onchange: '&'
 		},
-		controller: function ($scope) {
+		controller: function ($scope, $timeout) {
+			$scope.selectedVal = $scope.dropdownItems[$scope.dropdownModel] || '';
 			$scope.change = function (e, item) {
 				e.preventDefault();
-				$scope.dropdownModel = item;
+				$scope.selectedVal = item.name
+				$scope.dropdownModel = item.id;
+				$scope.onchange && $timeout($scope.onchange);
 			}
 		},
 		template: '<div class="nwook-input-dropdown">'+
-					'<input type="text" name="{{inputName}}" ng-model="inputModel" />'+
+					'<input type="text" name="{{inputName}}" ng-model="inputModel" ng-required="{{required}}" />'+
 					'<div class="dropdown nwook-dropdown">'+
 						'<button class="dropdown-toggle" id="{{id}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'+
-							'<span>{{dropdownModel}}</span>'+
+							'<span>{{selectedVal}}</span>'+
 							'<span class="caret"></span>'+
 						'</button>'+
 						'<ul class="dropdown-menu" aria-labelledby="{{id}}">'+
-							'<li ng-repeat="item in dropdownItems"><a href="#" ng-click="change($event, item)">{{item}}</a></li>'+
+							'<li ng-repeat="item in dropdownItems"><a href="#" ng-click="change($event, item)">{{item.name}}</a></li>'+
 						'</ul>'+
 					'</div>'+
 					'<input type="text" name="{{dropdownName}}" ng-model="dropdownModel" hidden ng-required="{{required}}" />'+
+				'</div>'
+	};
+}
+
+function nwookSelectableButton () {
+	return {
+		restrict: 'E',
+		scope: {
+			id: '@',
+			name: '@',
+			model: '=',
+			required: '=',
+			onclick: '&',
+			text: '@'
+		},
+		controller: function ($scope, $timeout) {
+			$scope.model = $scope.model || false;
+			$scope.change = function () {
+				$scope.model = !$scope.model;
+			}
+		},
+		template: '<div class="nwook-selectable-button" ng-class="{\'selected\': model}">'+
+					'<button type="button" ng-click="change()">{{text}}</button>'+
+					'<input type="checkbox" name="{{name}}" ng-model="model" hidden ng-required="{{required}}" />'+
 				'</div>'
 	};
 }
