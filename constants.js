581bc15090c1a3ea40e06eff53e22ee3ac7783da
Aravinder Reddy K <aravinder.k@gmail.com>
2017-11-02 16:29:34 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,3 @@
+var GOOGLE_API_KEY = 'AIzaSyAOdDnYThal1NZnCZZMZmzM5mHZYP4lsiM',
+GOOGLE_API_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
+GOOGLE_API_PLACE_DETAILS_URL = 'https://maps.googleapis.com/maps/api/place/details/json';
